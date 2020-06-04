9ce5c8eb02852e51d0f21c19099a90852d445161
yeyuqiang <yuqiang.ye@intel.com>
2020-06-04 10:30:59 +0800

    [POAE7-323] fix numa changes in RDD Cache (#1349)
    * fix numa changes in RDD Cache
    
    * Missing files for RDD Cache and packaing jar with dependencies
    
    * update readme

--- oap-spark/src/main/scala/org/apache/spark/SparkEnv.scala
+++ oap-spark/src/main/scala/org/apache/spark/SparkEnv.scala
@@ -176,7 +176,6 @@ object SparkEnv extends Logging {
     create(
       conf,
       SparkContext.DRIVER_IDENTIFIER,
-      None,
       bindAddress,
       advertiseAddress,
       Option(port),
@@ -195,7 +194,6 @@ object SparkEnv extends Logging {
   private[spark] def createExecutorEnv(
       conf: SparkConf,
       executorId: String,
-      numaNodeId: Option[String],
       hostname: String,
       numCores: Int,
       ioEncryptionKey: Option[Array[Byte]],
@@ -203,7 +201,6 @@ object SparkEnv extends Logging {
     val env = create(
       conf,
       executorId,
-      numaNodeId,
       hostname,
       hostname,
       None,
@@ -222,7 +219,6 @@ object SparkEnv extends Logging {
   private def create(
       conf: SparkConf,
       executorId: String,
-      numaNodeId: Option[String],
       bindAddress: String,
       advertiseAddress: String,
       port: Option[Int],
@@ -234,10 +230,14 @@ object SparkEnv extends Logging {
 
     val isDriver = executorId == SparkContext.DRIVER_IDENTIFIER
 
+    var numaNodeId = conf.getInt("spark.executor.numa.id", -1)
     val pmemInitialPaths = conf.get("spark.memory.pmem.initial.path", "").split(",")
     val pmemInitialSize = conf.getSizeAsBytes("spark.memory.pmem.initial.size", 0L)
     if (!isDriver && pmemInitialPaths.size > 1) {
-      val path = pmemInitialPaths(numaNodeId.getOrElse(executorId).toInt % 2)
+      if (numaNodeId == -1) {
+        numaNodeId = executorId.toInt
+      }
+      val path = pmemInitialPaths(numaNodeId % 2)
       val initPath = path + File.separator + s"executor_${executorId}" + File.pathSeparator
       val file = new File(initPath)
       if (file.exists() && file.isFile) {
